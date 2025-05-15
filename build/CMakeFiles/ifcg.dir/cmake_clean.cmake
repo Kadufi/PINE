c8993a3a3e6149c9b836e6bfff953466fe6f5ac8
kadufi <carlosperinifidelis@gmail.com>
2025-05-15 15:55:04 -0300

    mine

---
+++ build/CMakeFiles/ifcg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ifcg.dir/src/glad/glad.c.o"
+  "CMakeFiles/ifcg.dir/src/glad/glad.c.o.d"
+  "lib/libifcg.pdb"
+  "lib/libifcg.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ifcg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/main-test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main-test.dir/tests/main.cpp.o"
+  "CMakeFiles/main-test.dir/tests/main.cpp.o.d"
+  "CMakeFiles/main-test.dir/tests/texture.cpp.o"
+  "CMakeFiles/main-test.dir/tests/texture.cpp.o.d"
+  "bin/main-test"
+  "bin/main-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
